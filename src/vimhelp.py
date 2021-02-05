79736538b7b564b2ea267193686f1d743942840e
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2021-02-05 16:08:37 +0900

    add noexists word test case

--- src/vimhelp.py
+++ src/vimhelp.py
@@ -2,6 +2,8 @@ import subprocess
 
 
 def vimhelp(search_word):
-    help_index = subprocess.run(["node", "../vimhelp.js", search_word],
+    help_index = subprocess.run(["node", "--unhandled-rejections=strict",
+                                 "../vimhelp.js", search_word],
                                 encoding='utf-8', stdout=subprocess.PIPE)
+
     return help_index.stdout, help_index.returncode
--- test/test.py
+++ test/test.py
@@ -6,9 +6,11 @@ import vimhelp
 
 class TestVimhelp(unittest.TestCase):
 
-    def test_vimhelp_method(self):
+    def test_vimhelp_exist_word(self):
         self.assertEqual(0, vimhelp.vimhelp("search")[1])
 
+    def test_vimhelp_noexist_word(self):
+        self.assertEqual(1, vimhelp.vimhelp("noexists")[1])
 
 if __name__ == "__main__":
     unittest.main()
